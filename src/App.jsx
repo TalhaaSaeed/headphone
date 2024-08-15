e0e3779852646b7c7d39067bdecd7c0953cadca8
Talha Saeed <talhaq092@gmail.com>
2024-08-15 09:34:52 -0700

    Features

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import NavBar from "../src/components/NavBar/NavBar";
+import Hero from "../src/components/Hero/Hero";
+
+const App = () => {
+  return (
+    <>
+      <main className="overflow-x-hidden">
+        <NavBar />
+        <Hero />
+      </main>
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,27 @@
+const headphoneData = [
+  {
+    id: 1,
+    title: "Headphone",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean in magna ac nisi scelerisque tincidunt. Nulla facilisi.",
+    price: "$199",
+    model: "Model Brown",
+    bgColor: "#8b5958",
+  },
+];
+
+const Hero = () => {
+  return (
+    <>
+      <section className="bg-brandDark text-white">
+        <div className="container grid grid-cols-1 md:grid-cols-1 min-h-[700px]">
+          {/* Headphone Info */}
+          {/* Hero Image */}
+          {/* Whatsapp Icon */}
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Hero;
